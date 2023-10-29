715290719e59e5123132cf65e9bcdd608f833298
amalia-ullman <mali.ullman@gmail.com>
2023-10-29 12:03:02 -0700

    project structure and dynamic list beginning

---
+++ script.js
@@ -0,0 +1,9 @@
+document.write("hello")
+
+function createGreeting(name){
+    const greetingHeader = document.createElement("h1");
+    greetingHeader.innerText = "Greetings, " + name;
+    document.querySelector('body').appendChild(greetingHeader);
+}
+
+createGreeting(prompt("Enter your name: "));
