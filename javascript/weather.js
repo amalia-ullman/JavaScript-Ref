7bafb6129153e78015f00addb1f906a1da2e1cc9
amalia-ullman <mali.ullman@gmail.com>
2023-12-10 12:28:59 -0800

    weather js

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,8 +1,30 @@
 async function fetchWeatherData(){
-    const requestURL =  "https://api.weatherapi.com/v1/current.json?key=985a2db26c794b5ab87185757231012&q=89141";
+    const location = document.getElementById("searchInput").value;
+    const requestURL =  "https://api.weatherapi.com/v1/current.json?key=985a2db26c794b5ab87185757231012&q=" + location;
     const request = new Request(requestURL);
     const response = await fetch(request);
     const data = await response.json();
-    console.log(data);
 
+    if(data.error){
+        alert(`Unable to complete request. ${data.error.message}`);
+    }
+    else{
+        populateWeatherCard(data);
+    }
+
+}
+function populateWeatherCard(obj){
+    const card = document.getElementById('card');
+    const city = document.getElementById('city');
+    const temp = document.getElementById('temp');
+    const forecast = document.getElementById('forecast');
+    const icon = document.getElementById('icon');
+    const humidity = document.getElementById('humidity');
+
+    card.style.visibility="visible";
+    city.textContent = obj.location.name;
+    temp.textContent = obj.current.feelslike_f + "°F";
+    forecast.textContent = obj.current.condition.text;
+    icon.src = obj.current.condition.icon;
+    humidity.textContent = `Humidity: ${obj.current.humidity}%`
 }
