5b4bf7a450b933adef1ec895fd8f47424a977218
amalia-ullman <mali.ullman@gmail.com>
2023-12-24 12:00:32 -0800

    weather bg color

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,26 +1,21 @@
 async function fetchWeatherData(){
+
+    const body = document.getElementById("body");
+    body.classList.add('day');
     
-    const errorbadge = document.getElementById("error-badge");
-
-    if(errorbadge.classList.contains('visible')){
-        errorbadge.classList.remove('visible');
-        errorbadge.classList.add('invisible');
-    }
-
     const location = document.getElementById("searchInput").value;
     const requestURL =  "https://api.weatherapi.com/v1/current.json?key=985a2db26c794b5ab87185757231012&q=" + location;
+    fetchAstroData(location);
     const request = new Request(requestURL);
     const response = await fetch(request);
     const data = await response.json();
 
     if(data.error){
         // console.error(`Unable to complete request. ${data.error.message}`);
-        if(errorbadge.classList.contains('invisible')){
-            errorbadge.classList.remove('invisible');
-            errorbadge.classList.add('visible');
-        }
+        setErrorBadgeTo('visible');
     }
     else{
+        setErrorBadgeTo('invisible');
         populateWeatherCard(data);
     }
 
@@ -39,4 +34,40 @@ function populateWeatherCard(obj){
     forecast.textContent = obj.current.condition.text;
     icon.src = obj.current.condition.icon;
     humidity.textContent = `Humidity: ${obj.current.humidity}%`
+}
+
+function setErrorBadgeTo(visibility){
+    const errorbadge = document.getElementById("error-badge");
+
+    let v = (visibility == 'visible' ? 'invisible' : 'visible');
+
+    if(errorbadge.classList.contains(v)){
+        errorbadge.classList.remove(v);
+        errorbadge.classList.add(visibility);
+    }
+}
+
+async function fetchAstroData(location){
+    const requestURL = "http://api.weatherapi.com/v1/astronomy.json?key=985a2db26c794b5ab87185757231012&q=" + location;
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const data = await response.json();
+
+    setBackgroundColor(data);
+}
+
+function setBackgroundColor(obj){
+    const body = document.getElementById("body");
+    if(obj.astronomy.astro.is_sun_up == 1){
+        if(body.classList.contains('night')){
+            body.classList.remove('night');
+            body.classList.add('day');
+        }
+    }
+    else{
+        if(body.classList.contains('day')){
+            body.classList.remove('day');
+            body.classList.add('night');
+        }
+    }
 }
